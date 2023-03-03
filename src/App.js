02a71c71096a8ee8b88be05bd4d2f907ff22b5f2
Adithya7520 <adithya.upadhyaya007@gmail.com>
2023-03-03 21:42:46 +0530

    saved the changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import {Routes,Route,Link} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <Link to="/Page1">Page 1 </Link> 
+      <Link to="/Page2">Page 2 </Link>  
+
+{/* 1) to route between page we need to add browse router in index.js */}
+{/* 2) Define Routes and Route below or above the code which indicate that to where you have to go with that page tag */}
+{/* 3) use link to tag to travel to next page */}
+
+        <Routes>
+          <Route exact path='/Page1' element={<Page1/>}></Route>
+          <Route exact path='/Page2' element={<Page2/>}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ src/components/Page2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Page2() {
+  return (
+    <div>
+      <h2>Page 2</h2>
+    </div>
+  )
+}
